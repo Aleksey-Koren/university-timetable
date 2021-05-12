74de516b512bcd5b70dfe625616e93e4e545d55f
Aleksey <a.koren.dev@gmail.com>
2021-05-12 14:59:47 +0300

    Some commit

--- src/main/java/com/foxminded/koren/university/service/CourseServise.java
+++ src/main/java/com/foxminded/koren/university/service/CourseServise.java
@@ -1,5 +1,23 @@
 package com.foxminded.koren.university.service;
 
-public class CourseServise {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.foxminded.koren.university.dao.JdbcCourseDao;
+import com.foxminded.koren.university.domain.entity.Course;
+
+@Service
+public class CourseServise {
+    
+    @Autowired
+    private JdbcCourseDao jdbcCourseDao;
+    
+    public void createNewCourse(Course course) {
+        jdbcCourseDao.save(course);
+    }
+    
+    public void createNewCourse(String name, String description) {
+        Course course = new Course(name, description);
+        jdbcCourseDao.save(course);
+    } 
 }
