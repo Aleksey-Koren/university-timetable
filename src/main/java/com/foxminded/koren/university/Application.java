023f90ea90abbabfaab6cd3179a7a9758bafc281
Aleksey <a.koren.dev@gmail.com>
2021-10-15 14:15:48 +0300

    Changes in Spring Boot configuration. It still doesn't work

--- src/main/java/com/foxminded/koren/university/Application.java
+++ src/main/java/com/foxminded/koren/university/Application.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
